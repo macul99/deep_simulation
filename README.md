5ca4b4269681d3a1958dfe5eaf184f91aa7a8d48
macul99 <kunma9999@gmail.com>
2026-02-05 17:26:59 +0800

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+uv init
+uv venv --python 3.11
+uv pip install torch torchvision
+uv pip install torch_geometric
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from deep-simulation!")
+
+
+if __name__ == "__main__":
+    main()
